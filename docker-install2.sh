95fccc4b726ddc6dab61b9510c2e8936219f7410
sector <troythomps.sector@gmail.com>
2018-10-10 23:04:54 -0400

    old conky

---
+++ docker-install2.sh
@@ -0,0 +1,21 @@
+# sudo apt-get remove docker docker-engine docker.io -y
+# sudo apt-get update
+
+sudo apt-get install -y software-properties-common -y
+
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce -y
+
+sudo apt-cache madison docker-ce -y
+
+sudo docker run hello-world
